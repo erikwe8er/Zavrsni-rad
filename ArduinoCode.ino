112e0b0ec3aa519736c4acc6c4494f0e5fb5d7e8
Lovro Žižanović <149712635+LovroZizanovic@users.noreply.github.com>
2025-09-11 17:47:22 +0200

    Update ArduinoCode.ino

--- ArduinoCode.ino
+++ ArduinoCode.ino
@@ -1,8 +1,14 @@
 #include <Servo.h>
+#uključujemo biblioteku za upravljanje servo motorima
 
+#deklariramo polje od 5 servo objekata
 Servo servos[5];
+
+#pinovi Arduina na koje su spojeni servo motori
 int pins[5] = {6, 5, 2, 12, 11};
 
+
+#pokrećemo serijsku komunikaciju i svaki servo objekt povezujemo sa pripadajućim pinom
 void setup() {
   Serial.begin(9600);
   for (int i = 0; i < 5; i++) {
@@ -10,17 +16,20 @@ void setup() {
   }
 }
 
+#provjeravamo ima li podataka koji su stigli preko serijske veze
 void loop() {
   if (Serial.available() > 0) {
     String input = Serial.readStringUntil('\n');
     int lastComma = -1;
     int index = 0;
 
+    # parsiramo brojeve iz stringa (razdvojene zarezima), pretvaramo ih u kut (×20) i šaljemo na servo motore
     for (int i = 0; i < input.length(); i++) {
       if (input[i] == ',' || i == input.length() - 1) {
         String numStr = input.substring(lastComma + 1, i + 1);
         int angle = numStr.toInt() * 20;
 
+        #šaljemo izračunati kut odgovarajućem servo motoru
         servos[index].write(angle); 
         index++;
         lastComma = i;
